327a52326c65ccabf6f49a01c434b518622f5da8
Claire Wang <wangclaire07@gmail.com>
2025-01-21 14:50:23 -0500

    Updated frontend: fixed the fetching TLE from API issue

--- satellitevisualizer-frontend/src/App.js
+++ satellitevisualizer-frontend/src/App.js
@@ -1,21 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Viewer } from 'resium';
 import TLEDisplay from './components/TLEDisplay';
+import './App.css';
 import * as Cesium from 'cesium';
 
 const App = () => {
-    const noradId = 25544; // example norad id for the iss
+    // const noradId = 25544; // example norad id for the iss
 
     console.log("rendering");
+
+    const [noradId, setNoradId] = React.useState(25544);
+    const [inputValue, setInputValue] = useState(''); 
+    const viewer = React.useRef(null);
+
+    const handleInputChange = (event) => { 
+        setInputValue(event.target.value); 
+    }; 
+    
+    const handleSubmit = (event) => { 
+        event.preventDefault(); 
+        const parsedId = parseInt(inputValue, 10); 
+        if (!isNaN(parsedId)) { 
+            setNoradId(parsedId); 
+        } 
+    };
+
     return (
-        <div>
-            <h1>Satellite Visualizer</h1>
-            <div style={{ height: '500px', width: '100%' }} >
-                <Viewer full>
-                    {/* Cesium objects go here */}
+        <div className="app-container">
+            <div className="sidebar">
+                <h1>Satellite Visualizer</h1>
+                <form onSubmit={handleSubmit}> 
+                    <input 
+                        type="text" 
+                        value={inputValue} 
+                        onChange={handleInputChange} 
+                        placeholder="Enter NORAD ID" 
+                    /> 
+                    <button type="submit">Submit</button> 
+                </form>
+                <TLEDisplay noradId={noradId} />
+            </div>
+            <div className="viewer-container">
+                <Viewer ref={viewer} homeButton={false}>
+                    {/* <button
+                        onClick={() => {
+                            viewer.current.cesiumElement.camera.flyHome();
+                        }}>
+                        Home
+                    </button> */}
                 </Viewer>
             </div>
-            <TLEDisplay noradId={noradId} />
         </div>
     );
 };
