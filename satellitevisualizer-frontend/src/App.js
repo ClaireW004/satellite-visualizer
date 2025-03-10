112ea1b21a550ab87cd745db1288ed04d9568cdc
mtrats <tratsevskam@ufl.edu>
2025-03-10 17:04:05 -0400

    Revert "Merged Fetch TLE and Submit Buttons and Updated Button CSS"
    This reverts commit 545eb272ff5056368e0675a2f8159d4fc3bfbe16.

--- satellitevisualizer-frontend/src/App.js
+++ satellitevisualizer-frontend/src/App.js
@@ -3,6 +3,9 @@ import { Viewer } from 'resium';
 import TLEDisplay from './components/TLEDisplay';
 import './App.css';
 import * as Cesium from 'cesium';
+import axios from 'axios';
+
+
 
 const App = () => {
     // const noradId = 25544; // example norad id for the iss
@@ -11,7 +14,21 @@ const App = () => {
 
     const [noradId, setNoradId] = React.useState(25544);
     const [inputValue, setInputValue] = useState(''); 
+    const [tle, setTle] = useState("");
+    const [error, setError] = useState("");
     const viewer = React.useRef(null);
+
+    const fetchTLE = async () => {
+        try {
+            const response = await axios.get(`http://localhost:8080/api/satellite/${noradId}/tle`);
+            console.log(response.data);
+            setTle(response.data);
+            setError('');
+        } catch (err) {
+            setError('Failed to fetch TLE data. Satellite not found or server error.');
+            setTle('');
+        }
+    };
 
     const handleInputChange = (event) => { 
         setInputValue(event.target.value); 
@@ -23,13 +40,14 @@ const App = () => {
         if (!isNaN(parsedId)) { 
             setNoradId(parsedId); 
         } 
+        fetchTLE(noradId);
     };
 
     return (
         <div className="app-container">
             <div className="sidebar">
                 <h1>Satellite Visualizer</h1>
-                <form onSubmit={handleSubmit}> 
+                <form onSubmit={handleSubmit} className='input-group'> 
                     <input 
                         type="text" 
                         value={inputValue} 
@@ -38,7 +56,7 @@ const App = () => {
                     /> 
                     <button type="submit">Submit</button> 
                 </form>
-                <TLEDisplay noradId={noradId} />
+                <TLEDisplay tle={tle} error={error}/>
             </div>
             <div className="viewer-container">
                 <Viewer ref={viewer} homeButton={false}>
