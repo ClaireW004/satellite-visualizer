0d3bc4fcf19cc2deade291007cebd8ff5b1527c4
rehamansoor <reha.mansoor@ufl.edu>
2025-04-06 18:41:28 -0400

    Title added

--- satellitevisualizer-frontend/src/App.js
+++ satellitevisualizer-frontend/src/App.js
@@ -1,57 +1,77 @@
-import React, { useState } from 'react';
-import { Viewer } from 'resium';
-import TLEDisplay from './components/TLEDisplay';
-import logo from './cesiumLogo.png';
-import './App.css';
-import * as Cesium from 'cesium';
-
-const App = () => {
-    // const noradId = 25544; // example norad id for the iss
-
-    console.log("rendering");
-
-    const [noradId, setNoradId] = React.useState(25544);
-    const [inputValue, setInputValue] = useState(''); 
-    const viewer = React.useRef(null);
-
-    const handleInputChange = (event) => { 
-        setInputValue(event.target.value); 
-    }; 
-    
-    const handleSubmit = (event) => { 
-        event.preventDefault(); 
-        const parsedId = parseInt(inputValue, 10); 
-        if (!isNaN(parsedId)) { 
-            setNoradId(parsedId); 
-        } 
-    };
-
-    return (
-        <div className="app-container">
-            <div className="sidebar">
-                <div className="logo-title">
-                    <h1 >SATELLITE VISUALIZER</h1>
-                    <img src={logo} alt="Logo" className="sidebar-logo" />
+import React, { useState, useRef, useEffect } from 'react';
+    import { Viewer, Entity } from 'resium';
+    import * as Cesium from 'cesium';
+    import TLEDisplay from './components/TLEDisplay';
+    import cesiumLogo from './cesiumLogo.png';
+    import './App.css';
+    const App = () => {
+        // const noradId = 25544; // example norad id for the iss
+        const [noradId, setNoradId] = React.useState(25544);
+        const [inputValue, setInputValue] = useState('');
+        const viewerRef = useRef(null);
+        const handleInputChange = (event) => {
+            setInputValue(event.target.value);
+        };
+        const handleSubmit = (event) => {
+            event.preventDefault();
+            const parsedId = parseInt(inputValue, 10);
+            if (!isNaN(parsedId)) {
+                setNoradId(parsedId);
+            }
+        };
+        return (
+            <div className="app-container">
+                <div className="sidebar">
+                    <div className="logo-title">
+                       
+                        <img src={cesiumLogo} alt="Logo" className="sidebar-logo"/>
+                        <div>
+                            <h1 className= "satellite-text">SATELLITE<br /></h1>
+                            <h1 className= "visualizer-text">VISUALIZER</h1>
+                        </div>
+                    </div>
+        
+                    <form onSubmit={handleSubmit}>
+                        <input
+                            type="text"
+                            value={inputValue}
+                            onChange={handleInputChange}
+                            placeholder="Enter NORAD ID"
+                        />
+                        <button type="submit">Submit</button>
+                    </form>
+                    <TLEDisplay noradId={noradId} />
                 </div>
-                <form onSubmit={handleSubmit}> 
-                    <input 
-                        type="text" 
-                        value={inputValue} 
-                        onChange={handleInputChange} 
-                        placeholder="Enter NORAD ID" 
-                    /> 
-                    <button type="submit">Submit</button> 
-                </form>
-                <TLEDisplay noradId={noradId} />
+                <div className="viewer-container">
+                <Viewer ref={viewerRef} homeButton={false}>
+                        <Entity
+                            name="Test Point 1"
+                            position={Cesium.Cartesian3.fromDegrees(-82.34435030956476, 29.6482998, 31)}
+                            point={{ pixelSize: 10,
+                                color: Cesium.Color.RED,
+                                outlineColor: Cesium.Color.WHITE,
+                                outlineWidth: 2 }}>
+                        </Entity>
+                        <Entity
+                            name="Test Point 2"
+                            position={Cesium.Cartesian3.fromDegrees(2.2955342, 48.8580382, 41)}
+                            point={{ pixelSize: 10,
+                                color: Cesium.Color.RED,
+                                outlineColor: Cesium.Color.WHITE,
+                                outlineWidth: 2 }}>
+                        </Entity>
+                    </Viewer>
+                </div>
             </div>
-            <div className="viewer-container">
-                <Viewer ref={viewer} homeButton={false}>
-                    {/* Add other Cesium components here
-                    */}
-                </Viewer>
-            </div>
-        </div>
-    );
-};
+        );
+    };
+    export default App;
 
-export default App;
+
+
+
+
+
+
+
+
